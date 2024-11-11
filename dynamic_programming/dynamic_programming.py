5c0ba6b4658ada8d90ed35da6d2c317472c84f99
wxbj <weizhen20010616@163.com>
2024-11-11 19:05:10 +0800

    finite

--- dynamic_programming/dynamic_programming.py
+++ dynamic_programming/dynamic_programming.py
@@ -3,6 +3,14 @@ from collections import deque
 
 
 def dynamic_programming(n, b, W, V):
+    """
+
+    :param n: 物品数
+    :param b: 重量
+    :param W: 每个物品的重量
+    :param V: 每个物品的价值
+    :return: 选择的物品
+    """
     F = [[0 for _ in range(b)] for _ in range(n)]
     I = [[0 for _ in range(b)] for _ in range(n)]
     X = [0 for _ in range(n)]
@@ -30,58 +38,7 @@ def dynamic_programming(n, b, W, V):
             x = x - W[k]
         else:
             X[k] = 0
-    print(X)
-
-
-def dynamic_programming_use_pretreatment(n, b, W, V, pretreatment_list):
-    F = [[0 for _ in range(b + 1)] for _ in range(n + 1)]
-    I = [[0 for _ in range(b + 1)] for _ in range(n + 1)]
-    X = [0 for _ in range(n + 1)]
-    for k, my_set in zip(range(len(pretreatment_list)), pretreatment_list):
-        for x in my_set:
-            if x >= W[k] and V[k] + F[k - 1][x - W[k]] >= F[k - 1][x]:
-                F[k][x] = V[k] + F[k - 1][x - W[k]] >= F[k - 1][x]
-                I[k][x] = 1
-            else:
-                F[k][x] = F[k - 1][x]
-                I[k][x] = 0
-
-    x = b
-    for k in range(n, 0, -1):
-        if I[k][x] == 1:
-            X[k] = 1
-            x = x - W[k]
-        else:
-            X[k] = 0
-    print(X[1:])
-
-
-# 预处理，获取需要计算的值的序号
-def pretreatment(n, b, W):
-    # 用来存放预处理的二维列表
-    my_list = []
-    # 用来遍历物品
-    queue = deque()
-    # 初始化这个二维数组
-    for i in range(n + 1):
-        my_list.append(set())
-    # 将最后一个物品放入,初始化
-    queue.append(n)
-    queue.append(b)
-    for i in range(n, 0, -1):
-        while True:
-            j = queue.popleft()
-            k = queue.popleft()
-            if k > 0:
-                my_list[j].add(k)
-                queue.append(j - 1)
-                queue.append(k - W[j])
-            queue.append(j - 1)
-            queue.append(k)
-            if queue[0] != i:
-                break
-    # [set(), {0, 3, 6, 7, 10}, {0, 10, 3, 6}, {10, 3}, {10}]
-    return my_list
+    return X
 
 
 if __name__ == "__main__":
@@ -89,4 +46,4 @@ if __name__ == "__main__":
     b = 1000
     W = [random.randint(0, 100) for _ in range(100)]
     V = [random.randint(0, 100) for _ in range(100)]
-    dynamic_programming(n, b, W, V)
+    print(dynamic_programming(n, b, W, V))
